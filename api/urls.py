82277bf308c40c9dfa5152844224db5401ded3f6
AslanBe1 <aslanabdimuminov31@gmail.com>
2025-03-16 18:31:53 +0500

    API

--- api/urls.py
+++ api/urls.py
@@ -1,37 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from api import views
 
 app_name = 'customers'
 
-urlpatterns = [
-    path('', views.ProductList.as_view(), name='APi'),
-    path('<int:pk>/', views.ProductDetailView.as_view(), name='ApiDetail'),
+router = DefaultRouter()
+router.register(r'products', views.ProductViewSet)
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'comments', views.CommentViewSet)
 
-#    Generic
-
-    path('generic-api/', views.ProductLists.as_view(), name='GenericAPI'),
-    path('generic-details/<int:pk>/', views.ProductDetails.as_view(), name='GenericDetailsAPI'),
-
-#    Category
-
-    path('category-list/', views.CategoryList.as_view(), name='CategoryList'),
-    path('category-details/<int:pk>/', views.CategoryDetail.as_view(), name='CategoryDetails'),
-
-#    Comment
-    path('comment/', views.CommentsLists.as_view(), name='Comment'),
-    path('comment/<int:pk>/',views.CommentListsByProduct.as_view(), name='CommentsByProduct'),
-    path('comment-deteil/<int:pk>/', views.CommentDetailByGeneric.as_view(), name='CommentsDetail'),
-
-#   Tuitkf
-
-    # path('tuitkf/', views.ExitAPI.as_view(), name='tuitkf'),
-
-#   Create and Update Old
-
-    # path('product-create/', views.ProductCreateView.as_view(), name='ProductCreate'),
-    path('product-update/<int:pk>/', views.ProductUpdateView.as_view(), name='ProductUpdate'),
-    # path('generic-create/', views.ProductCreates.as_view(), name='GenericCreate'),
-    # path('generic-update/<int:pk>/', views.ProductUpdate.as_view(), name='GenericUpdate'),
-    # path('category-create/',views.CategoryCreates.as_view(), name='CategoryCreates'),
+urlpatterns = [ 
+    path('',include(router.urls)),
 
 ]
